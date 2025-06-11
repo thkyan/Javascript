3979a5c159e194869d5d64dfe95aba585a5850b2
Thayná de Matos Sousa <thaynamatos956@gmail.com>
2025-06-11 17:18:21 -0300

    up

--- primeiroCodigo.js
+++ primeiroCodigo.js
@@ -1,1 +1,11 @@
-console.log("Olá mundo!");
+const mensagemTela = "Oi tudo bem? Me chamo Thayná";
+let mensagemTela2 = "Agora posso mudar essa variavel, pois não é uma const";
+
+console.log(mensagemTela);
+console.log(mensagemTela);
+console.log(mensagemTela);
+console.log(mensagemTela2);
+console.log(mensagemTela2);
+
+mensagemTela2 = "Oi denovo";
+console.log(mensagemTela2);
